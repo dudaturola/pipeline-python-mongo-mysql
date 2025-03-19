ebd03caa9bf96eacf6aa61e4bb0dbd83feecbdc9
Duda Turola <idturola@gmail.com>
2025-03-19 14:54:16 -0300

    Primeiro commit: adicionando pipeline Python e dados JSON

---
+++ scripts/extract_and_save_data.py
@@ -0,0 +1,62 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import requests
+
+def conect_mongo(uri):
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    return client
+
+def create_connect_db(client, db_name):
+    db = client[db_name]
+    return db
+
+def create_connect_collection(db, col_name):
+    collection = db[col_name]
+    return collection
+
+import requests
+
+def extract_api_data(url):
+    response = requests.get(url)
+    
+    print(f"Status Code: {response.status_code}")  # Exibe o status da requisição
+    print(f"Resposta da API (primeiros 500 caracteres): {response.text[:500]}")  # Exibe o conteúdo da resposta
+
+    if response.status_code != 200:  # Verifica se a resposta foi bem-sucedida
+        raise Exception(f"Erro ao acessar API: {response.status_code}")
+    
+    try:
+        return response.json()  # Tenta converter para JSON
+    except requests.exceptions.JSONDecodeError:
+        raise Exception("Erro ao decodificar JSON, resposta vazia ou inválida.")
+
+
+
+def insert_data(col, data):
+    docs = col.insert_many(data)
+    n_docs_inseridos = len(docs.inserted_ids)
+    return n_docs_inseridos
+
+if __name__ == "__main__":
+
+    client = conect_mongo("mongodb+srv://dudaturola:<SUA SENHA AQUI>@cluster-pipeline.3kxud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-pipeline")
+    db = create_connect_db(client,"db_produtos_desafio")
+    col = create_connect_collection(db, "produtos")
+
+    data = extract_api_data("https://labdados.com/produtos")
+    print(f"\nQuantidade de dados extraidos: {len(data)}")
+
+    n_docs = insert_data(col, data)
+    print(f"\nDocumentos inseridos na colecao: {n_docs}")
+
+    client.close()
+
---
+++ scripts/script_extract_data_json_duda.py
@@ -0,0 +1,66 @@
+import kagglehub
+import pandas as pd
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import json
+
+# Baixar o dataset 
+dataset_path = kagglehub.dataset_download("mahmoudelhemaly/students-grading-dataset")
+
+# Ajuste o nome do arquivo conforme o dataset que vai importar
+file_path = f"{dataset_path}/Students_Grading_Dataset.csv"
+
+# Carregar o dataset via pandas
+df = pd.read_csv(file_path)
+
+print("Primeiras 5 linhas:", df.head())
+
+# TRANSFORMANDO EM JSON E SALVANDO NA PASTA DATA_JSON
+file_path_json = "../data_json/Students_Grading_Dataset.csv.json"
+df.to_json(file_path_json, orient="records",indent=4)
+print(f"Arquivo JSON salvo em: {file_path_json}")
+
+def conect_mongo(uri):
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    return client
+
+def creat_connect_db(client,db_name):
+    db = client[db_name]
+    return db
+
+def create_connect_collection(db,col_name):
+    collection = db[col_name]
+    return collection
+
+#UTILIZAMOS PARA PEGAR URL
+# def extract_api_data(url):
+#     response = response.get(url)
+#     print(f"Status Code: {response.status_code}")  # Exibe o status da requisição
+
+def extract_api_data(file_path):
+    with open(file_path,'r') as file:
+        data = json.load(file)
+    return data
+
+def insert_data(col,data):
+    docs = col.insert_many(data)
+    n_docs_inseridos = len(docs.inserted_ids)
+    return n_docs_inseridos
+
+if __name__ == "__main__":
+    client = conect_mongo("mongodb+srv://dudaturola:<SUA SENHA AQUI>@cluster-pipeline.3kxud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-pipeline")
+    db = creat_connect_db(client,"Dados_Studients")
+    col = create_connect_collection(db,"Studients")
+    data = extract_api_data("../data_json/Students_Grading_Dataset.csv.json")
+    print(f"\nQuantidade de dados extraidos: {len(data)}")
+    n_docs = insert_data(col,data)
+    print(f"\nDocumentos inseridos na colecao: {n_docs}")
+
+client.close()
